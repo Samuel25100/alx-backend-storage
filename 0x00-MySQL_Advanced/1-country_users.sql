debef09ee4d675429b61e0ca3bec9dab9af7803b
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-07 08:35:56 +0000

    add file 1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- creates a table users with id, email, name and country attribute
+CREATE DATABASE IF NOT EXISTS holberton;
+USE holberton;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') NOT NULL
+);
