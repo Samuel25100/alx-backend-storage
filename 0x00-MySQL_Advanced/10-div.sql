318dfa0f47b9dee1d8111b342507fa5e0cd87e36
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-08 20:48:30 +0300

    fixing file 10-div

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,6 +1,5 @@
 -- func divides (and returns) the first by the second number or returns 0
 -- if the second number is equal to 0.
-USE holberton;
 
 DELIMITER //
 CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS FLOAT
