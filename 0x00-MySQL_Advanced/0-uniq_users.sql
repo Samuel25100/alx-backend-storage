a02e390bf16e065ea5d24bb5dc2dd04660f5ce11
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-08 20:29:27 +0300

    fixing file 0-uniq

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,4 @@
 -- creates a table users with id, email, and name attribute
-CREATE DATABASE IF NOT EXISTS holberton;
-USE holberton;
 CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
