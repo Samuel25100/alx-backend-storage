f402d3f498d7cd5753255c1715c48346cbe07626
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-12 19:25:39 +0300

    fixing file 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,11 +7,9 @@ def log_stat():
     """provides some stats about Nginx logs stored in MongoDB Database: logs"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginxcol = client.logs.nginx
-    count = nginxcol.count_documents({})
-    ar = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print(count, "logs")
-    print("Methods:")
-    for i in ar:
+    count = nginxcol.estimated_document_count()
+    print(count, "logs\nMethods:")
+    for i in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
         result = nginxcol.count_documents({'method': i})
         print(f"\tmethod {i}: ", result)
     stat = nginxcol.count_documents({'method': 'GET', 'path': '/status'})
