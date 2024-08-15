dfc28ea3634d252e4f24289a17b9f54e5368e97d
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-15 15:14:16 +0300

    add file exercise for task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,21 @@
 """Class: Cache"""
 import redis
 import uuid
+from functools import wraps
 from typing import Union, Callable, Optional
+
+
+def count_calls(method: Callable) -> Callable:
+    """method to count number Cache class methods call"""
+    k: str = method.__qualname__
+
+    @wraps(method)
+    def warpper(self, *args, **kwargs):
+        """wrapper for decorator"""
+        self._redis.incr(k)
+        result = method(self, *args, **kwargs)
+        return result
+    return warpper
 
 
 class Cache:
@@ -10,18 +24,19 @@ class Cache:
 
     def __init__(self):
         """Constructor"""
-        self.__redis = redis.Redis()
-        self.__redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """create key and store data in database by key"""
         key: str = str(uuid.uuid4())
-        self.__redis.set(key, data)
+        self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable[[str], str]]) -> str:
+    def get(self, key: str, fn: Optional[Callable[[str], str]] = None) -> str:
         """getter for key and decode data using fn"""
-        val = self.__redis.get(key)
+        val = self._redis.get(key)
         if (fn):
             return fn(val)
         return val
@@ -29,13 +44,13 @@ class Cache:
     def get_str(self, key: str) -> Union[str, None]:
         """getter for data in str type"""
         if (key):
-            val = self.__redis.get(key, str)
+            val = self._redis.get(key, str)
             return val
         return None
 
     def get_int(self, key: str) -> Union[str, None]:
         """getter for data in int type"""
         if (key):
-            val = self.__redis.get(key, int)
+            val = self._redis.get(key, int)
             return val
         return None
