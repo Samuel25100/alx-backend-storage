3e81b983ad3235d81fd323b1b5001e3fcc728a49
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-15 11:07:38 +0300

    fixing exercise.py for task 1,2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,28 +2,40 @@
 """Class: Cache"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
     """class that use redis to store variable"""
 
     def __init__(self):
+        """Constructor"""
         self.__redis = redis.Redis()
         self.__redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        key = str(uuid.uuid4())
+        """create key and store data in database by key"""
+        key: str = str(uuid.uuid4())
         self.__redis.set(key, data)
         return key
 
-    def get(self, key, fn):
+    def get(self, key: str, fn: Optional[Callable[[str], str]]) -> str:
+        """getter for key and decode data using fn"""
+        val = self.__redis.get(key)
+        if (fn):
+            return fn(val)
+        return val
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """getter for data in str type"""
         if (key):
-            val = self.__redis.get(key)
-            if (fn == None):
-                return val
-            elif (fn == int):
-                return int(val)
-            else:
-                return fn(val)
+            val = self.__redis.get(key, str)
+            return val
+        return None
+
+    def get_int(self, key: str) -> Union[str, None]:
+        """getter for data in int type"""
+        if (key):
+            val = self.__redis.get(key, int)
+            return val
         return None
