7539b3223c37889e44042add6921bfc349a50740
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-16 18:49:52 +0300

    add file web.py

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""function: get_page"""
+import requests
+from redis import Redis
+
+
+def get_page(url: str) -> str:
+    """Request HTML content of a particular URL and returns it"""
+    count: int = 0
+    re: str = requests.get(url)
+    r = Redis()
+    if (r.exists(f"count:{url}")):
+        count = int(r.get(f"count:{url}")) + 1
+    r.setex(f"count:{url}", 10, count)
+    print("count:", count)
+    return re
